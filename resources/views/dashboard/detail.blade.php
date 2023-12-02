375377aebf6ed981acf9a38110cdfc3e9702e5ef
Dimas Aryansyah <142388724+DimasAryansyah@users.noreply.github.com>
2023-12-02 14:43:03 +0700

    Update detail.blade.php
    Menambah navbar dan content

--- resources/views/dashboard/detail.blade.php
+++ resources/views/dashboard/detail.blade.php
@@ -3,63 +3,86 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="detail.css">
-    <title>Document</title>
+    <title>InfoUMKM.com</title>
     <link rel="stylesheet" href="{{url('assets/css/detail-style.css')}}">
 </head>
 <body>
-        <div class="detail-card">
-            <div class="detail">
-                <div class="left">
-                    <img src="./img/resto.webp" alt="resto">
-                    <div class="small-image">
-                        <img src="./img/resto.webp" alt="resto">
-                        <img src="./img/resto.webp" alt="resto">
-                        <img src="./img/resto.webp" alt="resto">
-                    </div>
-                </div>
-
-                <div class="right">
-                    <div id="title">
-                        <h1>Warung Kalimantan</h1>
-                    </div>
-                    <div class="desc">
-                        <h3>★ ★ ★ ★ ★ | 40 Penilian | Jl. Kalimantan No. 57</h3>
-                        <p>Lokasi : <a href="https://maps.app.goo.gl/X6oSW9FbT86SDNUR8">https://maps.app.goo.gl/X6oSW9FbT86SDNUR8</a></p>
-                        <p>Website : -</p>
-                        <p>Contact : -</p>
-                        <p>Deskripsi : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
-                    <button>Beri Komentar</button>
-                </div>
-            </div>
+    <header class="header">
+        <div class="logo">
+            <img src="./img/logo.png" alt="logo InfoUMKM.com">
+            <h3>INFOUMKM.COM</h3>
         </div>
-
-        <div class="produk">
-            <div class="title-produk">
-                <h1>Produk</h1>
-                <div class="list">
-                    <div class="list-item">
+        <nav class="menu">
+            <ul>
+                <li>HOME</li>
+                <li>UMKM</li>
+                <li>FAVORITE</li>
+                <li>ABOUT-US</li>
+                <li class="right">Daftar</li>
+                <li class="right" style="color: #087292; width: 5px;">|</li>
+                <li class="right">Masuk</li>
+            </ul>
+        </nav>
+    </header>
+    <main class="content">
+        <div class="header-content">
+            <h1>CARI INFO UMKM KAMU <br> HANYA DI <span style="color: #0F3555;">INFOUMKM.COM</span></h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br> sed do eiusmod tempor incididunt ut.</p>
+        </div>
+        <div class="main-content">
+            <div class="detail-card">
+                <div class="detail">
+                    <div class="left">
                         <img src="./img/resto.webp" alt="resto">
-                        <div class="name"><h2>Bakso</h2></div>
-                        <h3>Rp. 15.000</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <div class="small-image">
+                            <img src="./img/resto.webp" alt="resto">
+                            <img src="./img/resto.webp" alt="resto">
+                            <img src="./img/resto.webp" alt="resto">
+                        </div>
                     </div>
-                    <div class="list-item">
-                        <img src="./img/resto.webp" alt="resto">
-                        <div class="name"><h2>Ketoprak</h2></div>
-                        <h3>Rp. 15.000</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
-                    <div class="list-item">
-                        <img src="./img/resto.webp" alt="resto">
-                        <div class="name"><h2>Rendang</h2></div>
-                        <h3>Rp. 15.000</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    
+                    <div class="right">
+                        <div id="title">
+                            <h1>Warung Kalimantan</h1>
+                        </div>
+                        <div class="desc">
+                            <h3>★ ★ ★ ★ ★ | 40 Penilian | Jl. Kalimantan No. 57</h3>
+                            <p>Lokasi : <a href="https://maps.app.goo.gl/X6oSW9FbT86SDNUR8">https://maps.app.goo.gl/X6oSW9FbT86SDNUR8</a></p>
+                            <p>Website : -</p>
+                            <p>Contact : -</p>
+                            <p>Deskripsi : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <a href="#" class="button">Beri Komentar</a>
                     </div>
                 </div>
             </div>
+    
+            <div class="produk">
+                <div class="title-produk">
+                    <h1>Produk</h1>
+                    <div class="list">
+                        <div class="list-item">
+                            <img src="./img/resto.webp" alt="resto">
+                            <div class="name"><h2>Bakso</h2></div>
+                            <h3>Rp. 15.000</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div class="list-item">
+                            <img src="./img/resto.webp" alt="resto">
+                            <div class="name"><h2>Ketoprak</h2></div>
+                            <h3>Rp. 15.000</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div class="list-item">
+                            <img src="./img/resto.webp" alt="resto">
+                            <div class="name"><h2>Rendang</h2></div>
+                            <h3>Rp. 15.000</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
+    </main>
 </body>
 </html>
