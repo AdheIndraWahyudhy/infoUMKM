9acf3cafecc084c7a7d4d6b90a462c574d6bbe96
Aulia Augusta <94182446+auliaagst@users.noreply.github.com>
2023-12-11 18:36:22 +0700

    membuat header aktor admin

---
+++ resources/views/Test/header/admin.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+      integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link rel="stylesheet" href="./header.css">
+</head>
+<body>
+        <header class="header">
+            <div class="logo">
+                <img src="./img/logo.png" alt="logo InfoUMKM.com">
+                <h3>INFOUMKM.COM</h3>
+            </div>
+            <nav class="menu">
+                <ul>
+                    <li><a href="">Pengguna</a></li>
+                    <li><a href="">Laporan</a></li>
+                    <li><a href="">Profil</a></li>
+                    <li><a><i class="fa-solid fa-user"></i>Halo, Aulia</a></li>
+                </ul>
+            </nav>
+            <div class="main-sidebar">
+                <input type="checkbox" id="check">
+                <label for="check">
+                    <i class="fas fa-bars" id="open"></i>
+                </label>
+                <div class="sidebar">
+                    <label for="check">
+                        <i class="fas fa-bars" id="btn"></i>
+                    </label>
+                    <ul>
+                        <li><a href="">Pengguna</a></li>
+                        <li><a href="">Laporan</a></li>
+                        <li><a href="">Profil</a></li>
+                        <li><a><i class="fa-solid fa-user"></i>Halo, Aulia</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <main class="content">
+            <div class="header-content">
+                <h1>CARI INFO UMKM KAMU <br> HANYA DI <span style="color: #0F3555;">INFOUMKM.COM</span></h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br> sed do eiusmod tempor incididunt ut.</p>
+            </div>
+            <!-- konten seterusnya disini -->
+        </main>
+    </body>
+</html>
