a39e98c28af723e0d34b752caa7b380c82680406
VicoPratama <71577686+VicoPratama@users.noreply.github.com>
2023-12-11 08:10:58 +0700

    profil-logout.blade.php

--- resources/views/Test/profil-logout.blade.php
+++ resources/views/Test/profil-logout.blade.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>UMKM</title>
 
-        <link rel="stylesheet" href="css/reset.css" />
-        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/profil-reset.css" />
+        <link rel="stylesheet" href="css/profil-style.css" />
 
         <!-- font awesome  -->
         <link
